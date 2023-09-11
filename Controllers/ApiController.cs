14480ebd3708c6d855ae81b58836626b4da20c1e
luisf4 <luismarcelino.dev@gmail.com>
2023-09-11 14:25:39 -0300

    feat: search Games deals

--- Controllers/ApiController.cs
+++ Controllers/ApiController.cs
@@ -1,24 +1,27 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using gameDeal.Models;
+using System.Text.Json;
+
 
 namespace gameDeal.Controllers;
 
 public class ApiController : Controller {
 
-    private string apikey = "GEI0DCLRW3HGMQJQ";
 
-
-    public string Index()
-    {
-        return "Hello from Api";
-    }
-
-    public async Task<IActionResult> GetApiData() {
+    public async Task<IActionResult> Index() {
         var client = new HttpClient();
         var response = await client.GetAsync("https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=15");
-        var content = await response.Content.ReadAsStringAsync();
-        return Content(content);
-
+        
+        if (response.IsSuccessStatusCode) {
+            var content = await response.Content.ReadAsStringAsync();
+            var deals = JsonSerializer.Deserialize<List<Deal>>(content);
+            return View(deals);
+        }
+        else {
+            // Handle error response here
+            return View(new List<Deal>()); // You can also redirect to an error page
+        }
     }
+
 }
